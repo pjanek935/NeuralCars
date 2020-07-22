074ff548eb25c6817e5764e8eae9d3f3f7e81710
ppj-mobo <ppj@mobostudio.com>
2020-07-22 16:03:34 +0200

    dodanie klasy tooltipow

---
+++ Assets/Tooltip.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    public enum TooltipState
+    {
+        HIDDEN, VISIBLE, FADING_IN, FADING_OUT, WAITING_FOR_DISPLAY,
+    }
+
+    public enum TooltipAnchorPosition
+    {
+        CENTER, LEFT, RIGHT, UP, DOWN,
+    }
+
+    [SerializeField] Text text;
+    [SerializeField] CanvasGroup canvasGroup;
+
+    const float waitTime = 0.4f;
+    float timer = 0f;
+    float showRate = 3f;
+
+    public static Tooltip Instance
+    {
+        get;
+        private set;
+    }
+
+    public TooltipState State
+    {
+        get;
+        private set;
+    }
+
+    private void Awake ()
+    {
+        if (Instance != null)
+        {
+            Destroy (this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            State = TooltipState.HIDDEN;
+        }
+    }
+
+    private void OnDestroy ()
+    {
+        Instance = null;
+    }
+
+    public void Show (string message, RectTransform rectTransform)
+    {
+        if (rectTransform == null)
+        {
+            return;
+        }
+
+        this.transform.position = rectTransform.transform.position;
+        text.text = message;
+
+        switch (State)
+        {
+            case TooltipState.HIDDEN:
+
+                State = TooltipState.WAITING_FOR_DISPLAY;
+
+                break;
+
+            case TooltipState.FADING_OUT:
+
+                State = TooltipState.FADING_IN;
+
+                break;
+        }
+    }
+
+    public void Hide ()
+    {
+        switch (State)
+        {
+            case TooltipState.VISIBLE:
+            case TooltipState.FADING_IN:
+
+                State = TooltipState.FADING_OUT;
+
+                break;
+
+            case TooltipState.WAITING_FOR_DISPLAY:
+
+                State = TooltipState.HIDDEN;
+                timer = 0f;
+
+                break;
+        }
+    }
+
+    private void Update ()
+    {
+        switch (State)
+        {
+            case TooltipState.FADING_IN:
+
+                {
+                    float newAlpha = canvasGroup.alpha + showRate * Time.deltaTime;
+
+                    if (newAlpha >= 1f)
+                    {
+                        State = TooltipState.VISIBLE;
+                    }
+
+                    canvasGroup.alpha = newAlpha;
+                }
+
+                break;
+
+            case TooltipState.FADING_OUT:
+
+                {
+                    float newAlpha = canvasGroup.alpha - showRate * Time.deltaTime;
+
+                    if (newAlpha <= 0f)
+                    {
+                        State = TooltipState.HIDDEN;
+                    }
+
+                    canvasGroup.alpha = newAlpha;
+                }
+
+                break;
+
+            case TooltipState.HIDDEN:
+
+                break;
+
+            case TooltipState.VISIBLE:
+
+                break;
+
+            case TooltipState.WAITING_FOR_DISPLAY:
+
+                timer += Time.deltaTime;
+
+                if (timer >= waitTime)
+                {
+                    timer = 0;
+                    State = TooltipState.FADING_IN;
+                }
+
+                break;
+        }
+    }
+}
---
+++ Assets/TooltipTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent (typeof (RectTransform))]
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] string message;
+    [SerializeField] Tooltip.TooltipAnchorPosition anchorPosition = Tooltip.TooltipAnchorPosition.CENTER;
+
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        Tooltip.Instance.Show (message, (RectTransform) transform);
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+        Tooltip.Instance.Hide ();
+    }
+}
